4aebe20347e6bea10ded41b60049313fc9c26a3d
Bennet Meyers <bennetm@stanford.edu>
2017-11-19 13:06:10 -0800

    Working basic implementation of functional regression

--- core/problems.py
+++ core/problems.py
@@ -4,6 +4,7 @@ This module contains problem types that we are planning to address.
 """
 import numpy as np
 import pandas as pd
+from numpy.linalg import norm
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 
 class ManyToOneRegression(object):
@@ -141,8 +142,96 @@ class SumToSumARIMA(object):
         return np.sum(np.power(residuals, 2)) / np.float(len(residuals))
 
 class FunctionalRegression(object):
-    def __init__(self, df):
-        pass
+    def __init__(self, train, test, window=12*5, future=12*3, train_selection='all', test_selection='hourly'):
+        if train_selection == 'all':
+            self.train_features = np.array([train.iloc[i:i+window, :-1].values.ravel()
+                                            for i in xrange(len(train) - window - future)])
+            self.train_response = np.array([train.iloc[i+window:i+window+future, -1].values.ravel()
+                                            for i in xrange(len(train) - window - future)])
+        elif train_selection == 'hourly':
+            self.train_features = np.array([train.iloc[i:i + window, :-1].values.ravel()
+                                            for i in xrange(0, len(train) - window - future, 12)])
+            self.train_response = np.array([train.iloc[i + window:i + window + future, -1].values.ravel()
+                                            for i in xrange(0, len(train) - window - future, 12)])
+        if test_selection == 'all':
+            self.test_features = np.array([test.iloc[i:i+window, :-1].values.ravel()
+                                            for i in xrange(len(test) - window - future)])
+            self.test_response = np.array([test.iloc[i+window:i+window+future, -1].values.ravel()
+                                            for i in xrange(len(test) - window - future)])
+            self.test_response_ts = np.array([test.iloc[i+window:i+window+future, -1].index
+                                            for i in xrange(len(test) - window - future)])
+        elif test_selection == 'hourly':
+            self.test_features = np.array([test.iloc[i:i + window, :-1].values.ravel()
+                                            for i in xrange(0, len(test) - window - future, 12)])
+            self.test_response = np.array([test.iloc[i + window:i + window + future, -1].values.ravel()
+                                            for i in xrange(0, len(test) - window - future, 12)])
+            self.test_response_ts = np.array([test.iloc[i + window:i + window + future, -1].index
+                                           for i in xrange(0, len(test) - window - future, 12)])
+        self.train = train
+        self.test = test
+        self.window = window
+        self.future = future
+        self.distances = None
+        self. neighborhoods = None
+        self.forecasts = None
+
+    def make_forecasts(self, neighborhood=5):
+        self.calc_distances_and_neighborhoods(neighborhood=neighborhood)
+        p, n = self.test_features.shape
+        forecasts = []
+        forecasts_a = forecasts.append
+        for j in xrange(p):
+            neighb = self.neighborhoods[j]
+            ds = self.distances[j, neighb]
+            h = np.max(self.distances[j, :])
+            num = 0
+            den = 0
+            for i in neighb:
+                k = self.ker(self.distances[j, i] / h)
+                num += k * self.train_response[i, :]
+                den += k
+            est = pd.Series(data=(num / den), index=self.test_response_ts[j])
+            forecasts_a(est)
+        self.forecasts = forecasts
+
+    def calc_distance(self, feature1, feature2):
+        residual = feature1 - feature2
+        distance = np.sqrt(residual.dot(residual))
+        return distance
+
+    def calc_distances_and_neighborhoods(self, neighborhood=5):
+        m, n = self.train_features.shape
+        p, n = self.test_features.shape
+        d = [[self.calc_distance(self.test_features[j, :], self.train_features[i, :])
+              for i in xrange(m)] for j in xrange(p)]
+        n = [np.argsort(d[i])[:neighborhood] for i in xrange(p)]
+        self.distances = np.array(d)
+        self.neighborhoods = np.array(n)
+
+    def ker(self, t):
+        output = np.max(np.c_[1 - t, np.zeros_like(t)], axis=1)
+        if len(output) == 1:
+            return output[0]
+        else:
+            return output
+
+    def plot_test(self, start=None, end=None):
+        self.test.plot(y='total_power', linewidth=1.5, ls=':')
+        for ind, f in enumerate(self.forecasts):
+            series = pd.Series(data=f, index=self.test_response_ts[ind])
+            series.plot(linewidth=1)
+
+
+    def calc_mse(self):
+        residuals = np.array([])
+        for f in self.forecasts:
+            join = pd.concat([f, self.test], axis=1).dropna()
+            if np.max(join['total_power']) > 0:
+                r = join['total_power'] - join[0]
+                residuals = np.r_[residuals, r]
+        return np.sum(np.power(residuals, 2)) / np.float(len(residuals))
+
+
 
 if __name__ == "__main__":
     df = pd.read_pickle('/Users/bennetmeyers/Documents/CS229/Project/SolarForecasting/data/master_dataset.pkl')
