83f4e20a2874f38ab22d23c727d28a52bd71749f
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-12-07 20:27:35 -0800

    Add regularization option to nets

--- core/net_models.py
+++ core/net_models.py
@@ -1,17 +1,16 @@
 import tensorflow as tf
 from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
-from keras.regularizers import l1, l2
 
 
 class FC(object):
     """
     Fully-connected network
     """
-    def __init__(self, layer_units):
+    def __init__(self, layer_units, regularizer=None):
         nlayers = len(layer_units)
         self.layers = []
         for i in range(nlayers-1):
-            self.layers.append(Dense(layer_units[i], activation=tf.nn.relu))
+            self.layers.append(Dense(layer_units[i], activation=tf.nn.relu, kernel_regularizer=regularizer))
         self.layers.append(Dense(layer_units[-1]))
 
     def __call__(self, x):
@@ -28,13 +27,16 @@ class CNN(object):
     Given a flattened input example x, reshape it to an image with
     height and width and feed forward in the net.
     """
-    def __init__(self, layer_units, height, width):
+    def __init__(self, layer_units, height, width, regularizer=None):
         nlayers = len(layer_units)
         self.height = height
         self.width = width
         self.layers = []
         for i in range(nlayers-1):
-            self.layers.append(Conv2D(layer_units[i], kernel_size=5, activation=tf.nn.relu, padding='same'))
+            self.layers.append(Conv2D(layer_units[i], kernel_size=5,
+                                      activation=tf.nn.relu,
+                                      kernel_regularizer=regularizer,
+                                      padding='same'))
             self.layers.append(MaxPool2D(padding='same'))
         self.layers.append(Flatten())
         self.layers.append(Dense(layer_units[-1]))
