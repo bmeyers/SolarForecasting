003a31937a9e5da7a3864a019c8813404268a192
Bennet Meyers <bennetm@stanford.edu>
2017-12-08 17:04:26 -0800

    Turned the centering and scaling function into a class so it can fit to training data and be applied to dev/test data. Added centering and scaling to exogenous data.

--- core/preprocessing.py
+++ core/preprocessing.py
@@ -589,8 +589,8 @@ def make_batch(df, size, present, future, exo=False, randomize=True):
         x = features[t:t+present].values.T.flatten().tolist()
         y = response[t+present:t+present+future].values.tolist()
         if exo:
-            DoY = DoY_lookup[t+present]
-            ToD = ToD_lookup[t+present].hour * 12 + ToD_lookup[t+present].minute / 5
+            DoY = (DoY_lookup[t+present] - 183.) / 105.36602868097478
+            ToD = ((ToD_lookup[t+present].hour * 12 + ToD_lookup[t+present].minute / 5) - 143.5) / ﻿83.137937589686857
             x.extend([DoY, ToD])
         X.append(x)
         Y.append(y)
@@ -600,14 +600,23 @@ def make_batch(df, size, present, future, exo=False, randomize=True):
     Y = np.array(Y, dtype=np.float32)
     return X, Y
 
-def center_design_matrix(X):
+class CenterScale(object):
     """
     Center the rows of the design matrix.
     """
-    m = np.mean(X, 0)
-    s = np.std(X, 0)
-    Xcent = (X - m) / s
-    return Xcent
+    def __init__(self):
+        self.mean = None
+        self.stdev = None
+
+    def fit(self, x):
+        m = np.mean(x, axis=0)
+        s = np.std(x, axis=0)
+        self.mean = m
+        self.stdev = s
+
+    def scale(self, x):
+        Xcent = (x - self.mean) / self.stdev
+        return Xcent
 
 
 if __name__ == "__main__":
